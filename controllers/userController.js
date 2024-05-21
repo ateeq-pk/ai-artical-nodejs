33dbfcace5135fff8b7e96c664667b801f249dfc
Ateeq <119816896+ateeq-pk@users.noreply.github.com>
2024-05-21 05:30:18 +0500

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
-const User = require("../models/userModels")
+const {User} = require("../models/userModels")
 const jwt = require('jsonwebtoken')
+
 
 const createToken= (_id)=>{
 return jwt.sign({_id:_id},process.env.SECRET,{expiresIn:'3d'})
@@ -22,22 +23,17 @@ try {
 
 }
 
-//signup user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body
+    const { email, password } = req.body;
 
     try {
-        const user = await User.signup(email, password)
-
-        //create a token
-        const token=createToken(user._id)
-
-        res.status(200).json({ email, token })
+        const { user, otpResponse } = await User.signup(email, password);
+        res.status(200).json({ email, otpResponse });
     } catch (error) {
-        res.status(400).json({ error: error.message })
+        res.status(400).json({ error: error.message });
     }
+};
 
-}
 
 
-module.exports = { signupUser, loginUser }
+module.exports = { signupUser, loginUser ,createToken}
